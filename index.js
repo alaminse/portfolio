deba358c9a200f0bc7750cf53844700a10ae0298
alaminse <contactalamin.se@gmail.com>
2021-09-17 03:50:23 +0600

    1.0

---
+++ index.js
@@ -0,0 +1,175 @@
+$(document).ready(function(){
+
+    $(".button-1").click(function(){
+        $(".js-1").css({
+            "opacity" : "1"
+        });
+        $(".js-2").css({
+            "opacity" : "1"
+        });
+        $(".js-3").css({
+            "opacity" : "1"
+        });
+        $(".js-4").css({
+            "opacity" : "1"
+        });
+        $(".js-5").css({
+            "opacity" : "1"
+        });
+        $(".js-6").css({
+            "opacity" : "1"
+        });
+
+        $(".focus").css({
+            "outline": "0",
+            "color": "#212529",
+            "box-shadow": "none",
+            "box-shadow": "inset -2px -2px 6px rgba(255, 255, 255, 0.1),inset 2px 2px 6px rgba(0, 0, 0, 0.8)",    
+            "transform": "scale(0.99) !important"
+        });
+    });
+
+    
+    $(".button-2").click(function(){
+        $(".js-5").css({
+            "opacity" : "0.3"
+        });
+        $(".js-1").css({
+            "opacity" : "1"
+        });
+        $(".js-2").css({
+            "opacity" : "1"
+        });
+        $(".js-3").css({
+            "opacity" : "1"
+        });   
+        $(".js-4").css({
+            "opacity" : "1"
+        });
+        $(".js-6").css({
+            "opacity" : "0.3"
+        });
+        $(".focus").css({
+            "position": "relative",
+            "display": "flex",
+            "justify-content": "center",
+            "width": "100%",
+            "height": "30px",
+            "color": "white",
+            "padding": "0px 0px",
+            "margin": "12px",
+            "margin-bottom": "2px",
+            "background": "#131419",
+            "border": "none",
+            "border-radius": "25px",
+            "font-family": "Arial, black",
+            "font-weight": "600",
+            "font-size": "15px !important",
+            "padding-top": "4px !important",
+            "outline": "none !important",
+            "border-style": "none",
+            "box-shadow": "-2px -2px 6px rgba(255, 255, 255, 0.1),2px 2px 6px rgba(0, 0, 0, 0.8)"
+        });
+    });
+
+
+    $(".button-3").click(function(){
+        $(".js-4").css({
+            "opacity" : "0.3"
+        });
+        $(".js-2").css({
+            "opacity" : "0.3"
+        });
+        $(".js-5").css({
+            "opacity" : "1"
+        });
+        $(".js-1").css({
+            "opacity" : "1"
+        });
+        $(".js-3").css({
+            "opacity" : "1"
+        });
+        $(".js-6").css({
+            "opacity" : "1"
+        });
+        $(".focus").css({
+            "position": "relative",
+            "display": "flex",
+            "justify-content": "center",
+            "width": "100%",
+            "height": "30px",
+            "color": "white",
+            "padding": "0px 0px",
+            "margin": "12px",
+            "margin-bottom": "2px",
+            "background": "#131419",
+            "border": "none",
+            "border-radius": "25px",
+            "font-family": "Arial, black",
+            "font-weight": "600",
+            "font-size": "15px !important",
+            "padding-top": "4px !important",
+            "outline": "none !important",
+            "border-style": "none",
+            "box-shadow": "-2px -2px 6px rgba(255, 255, 255, 0.1),2px 2px 6px rgba(0, 0, 0, 0.8)"
+        });
+    });
+
+
+    $(".button-4").click(function(){
+        $(".js-2").css({
+            "opacity" : "0.3"
+        });
+        $(".js-3").css({
+            "opacity" : "0.3"
+        });
+        $(".js-4").css({
+            "opacity" : "1"
+        });
+        $(".js-5").css({
+            "opacity" : "1"
+        });
+        $(".js-6").css({
+            "opacity" : "1"
+        });
+        $(".js-1").css({
+            "opacity" : "1"
+        });
+        $(".focus").css({
+            "position": "relative",
+            "display": "flex",
+            "justify-content": "center",
+            "width": "100%",
+            "height": "30px",
+            "color": "white",
+            "padding": "0px 0px",
+            "margin": "12px",
+            "margin-bottom": "2px",
+            "background": "#131419",
+            "border": "none",
+            "border-radius": "25px",
+            "font-family": "Arial, black",
+            "font-weight": "600",
+            "font-size": "15px !important",
+            "padding-top": "4px !important",
+            "outline": "none !important",
+            "border-style": "none",
+            "box-shadow": "-2px -2px 6px rgba(255, 255, 255, 0.1),2px 2px 6px rgba(0, 0, 0, 0.8)"
+        });
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+
+    $(deoc1()).click(function(){
+        $("btns").css({
+            "outline": "0",
+            "color": "#212529",
+            "box-shadow": "none",
+            "box-shadow": "inset -2px -2px 6px rgba(255, 255, 255, 0.1),inset 2px 2px 6px rgba(0, 0, 0, 0.8)",
+            "transform": "scale(0.99) !important"
+
+        });
+    });
+
+});
